4cd79ffa6d1ffd7e5983ddebcb6311f3f305f6da
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-09-01 13:31:48 -0300

    adding fibonacci method with recursive implementation

---
+++ RecursiveExamples/Fibonacci.java
@@ -0,0 +1,13 @@
+
+public class Fibonacci {
+    public static int fibo(int n){
+        if(n == 0){
+            return 0;
+        }else {
+            if(n == 1){
+            return 1;
+            }
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
