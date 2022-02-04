28f368b2af4eca5a581e8bb521134d71b1765961
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-02-04 15:27:54 -0300

    Added Queue Data Structure

---
+++ DataStructures/Queues/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+            Console.WriteLine($"Length: {queue.Length}");
+            queue.Enqueue("Wesley");
+            queue.Enqueue("Luis");
+            queue.Enqueue("Vilma");
+            queue.Enqueue("Felipe");
+            Console.WriteLine($"Length: {queue.Length}");
+            Console.WriteLine($"Peek: {queue.Peek().Data}");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("ListQueue:");
+            queue.ListQueue();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("ListQueue:");
+            queue.Dequeue();
+            queue.ListQueue();
+            Console.WriteLine("--------------------");
+            queue.Dequeue();
+            queue.Dequeue();
+            Console.WriteLine("ListQueue:");
+            queue.ListQueue();
+        }
+    }
+}
+
---
+++ DataStructures/Queues/Queue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Queue
+    {
+        public Node First { get; set; }
+        public Node Last { get; set; }
+        public int Length { get; set; }
+
+        public Node Peek()
+        {
+            return First;
+        }
+
+        public void Enqueue(string data)
+        {
+            Node newNode = new Node(data);
+
+            if (Length == 0)
+            {
+                First = newNode;
+                Last = newNode;
+                Length++;
+            }
+            else
+            {
+                Last.Next = newNode;
+                Last = newNode;
+                Length++;
+            }
+        }
+
+        public void Dequeue()
+        {
+            First = First.Next;
+            Length--;
+        }
+
+        public void ListQueue()
+        {
+            Node temp = First;
+            while(temp.Next != null)
+            {
+                Console.WriteLine(temp.Data);
+                temp = temp.Next;
+            }
+            Console.WriteLine(temp.Data);
+        }
+    }
+}
