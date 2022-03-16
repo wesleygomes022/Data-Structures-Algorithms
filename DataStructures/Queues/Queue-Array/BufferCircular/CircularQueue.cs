ca4b392269b9deca5135ac725f18954cbdfcfc53
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-03-16 16:23:47 -0300

    Added Circular buffer implementation sample

---
+++ DataStructures/Queues/Queue-Array/BufferCircular/CircularQueue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace BufferCircular
+{
+    class CircularQueue
+    {
+        private int[] Ele;
+        private int Front;
+        private int Rear;
+        private int Max;
+        private int Count;
+
+        public CircularQueue(int size)
+        {
+            Ele = new int[size];
+            Front = 0;
+            Rear = -1;
+            Max = size;
+            Count = 0;
+        }
+
+        public void insert(int item)
+        {
+            if (Count == Max)
+            {
+                Console.WriteLine("Queue Overflow");
+                return;
+            }
+            else
+            {
+                Rear = (Rear + 1) % Max;
+                Ele[Rear] = item;
+
+                Count++;
+            }
+        }
+
+        public void delete()
+        {
+            if (Count == 0)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Console.WriteLine("deleted element is: " + Ele[Front]);
+
+                Ele[Front] = 0;
+                Front = (Front + 1) % Max;
+
+                Count--;
+            }
+        }
+
+        public void printQueue()
+        {
+            if (Count == 0)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            else
+            {
+                for(int i = 0; i < Max; i++)
+                {
+                    Console.WriteLine($"Item[{i}]: {Ele[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ DataStructures/Queues/Queue-Array/BufferCircular/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BufferCircular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircularQueue Q = new CircularQueue(5);
+
+            Q.insert(10);
+            Q.insert(20);
+            Q.insert(30);
+            Q.insert(40);
+            Q.insert(50);
+
+            Q.printQueue();
+
+            Console.WriteLine();
+            Q.delete();
+            Q.delete();
+
+            Console.WriteLine();
+            Q.printQueue();
+
+            Q.insert(60);
+
+            Console.WriteLine();
+            Q.printQueue();
+
+            Console.WriteLine();
+            Q.insert(70);           
+
+            Console.WriteLine();
+            Q.printQueue();
+            Console.WriteLine();
+            Q.insert(80);
+
+        }
+    }
+}
